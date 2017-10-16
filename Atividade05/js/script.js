5d4ccf18c955552afab529ecca6c6450423c3901
mariajesca <jescanobre@gmail.com>
2017-10-16 21:46:14 -0200

    ...

--- Atividade05/js/script.js
+++ Atividade05/js/script.js
@@ -1,7 +1,15 @@
-let butao = document.querySelector(".bt");
+let botao = document.querySelector(".bt");
 let navLateral = document.querySelector(".nav-lateral");
 
-function menuon() {
-    
-}
+botao.addEventListener('click', function() {
+     navLateral.style.display = "block";
+})
 
+
+
+// function clique() {
+// botao.classList.toggle("nav-lateral-on");
+// }
+
+// let botao = document.querySelector(".bt");
+// botao.addEventListener("click",clique);
